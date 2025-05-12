52d573b8998d69485a39fdcf7b1d72c7784ec4de
KeanuM123 <keanumoodley8@gmail.com>
2025-05-12 11:45:49 +0200

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -17,7 +17,7 @@ public class DemoApplication implements CommandLineRunner {
     }
 
     public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+        SpringApplication.run(DemoApplication.class, args); 
     }
 
     @Override
