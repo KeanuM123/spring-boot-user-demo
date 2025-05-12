4c412754235df785efdfd18b6733202dd0ea8830
KeanuM123 <keanumoodley8@gmail.com>
2025-05-12 11:46:21 +0200

    Update UserServiceTests.java

--- src/test/java/com/example/demo/service/UserServiceTests.java
+++ src/test/java/com/example/demo/service/UserServiceTests.java
@@ -2,7 +2,7 @@ package com.example.demo.service;
 
 import com.example.demo.repo.FakeRepo;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Test; 
 
 public class UserServiceTests {
 
